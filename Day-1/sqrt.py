087022e2fa49618e8551b606861c0c3238b01fe7
Sahitha0203 <sahitha.mutakodoor@gmail.com>
2025-06-11 00:12:26 +0530

    Update sqrt.py
    Solved LeetCode problem: Sqrt(x) using binary search in Python.
    Implemented binary search solution , optimized to return only the integer part of the square root.

--- Day-1/sqrt.py
+++ Day-1/sqrt.py
@@ -1,1 +1,15 @@
-
+class Solution(object):
+    def mySqrt(self, x):
+        low=0
+        high=x
+        while(low<=high):
+            mid=(low+high)//2
+            if(mid*mid==x):
+                return mid
+            elif(mid*mid<x):
+                low=mid+1
+            else:
+                high=mid-1
+        return high
+sol=Solution()
+print(sol.mySqrt(4))
