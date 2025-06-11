f0a0fa0cdf5aa283576e485d0ae634a77c962ffe
Sahitha0203 <sahitha.mutakodoor@gmail.com>
2025-06-11 23:14:23 +0530

    Update prefixSum1D.py

--- Day-2/prefixSum1D.py
+++ Day-2/prefixSum1D.py
@@ -1,1 +1,10 @@
+class Solution(object):
+    def runningSum(self, nums):
+        prefix=[0]*len(nums)
+        prefix[0]=nums[0]
+        for i in range(1,len(nums)):
+            prefix[i]=prefix[i-1]+nums[i]
+        return prefix
 
+sol=Solution()
+print(sol.runningSum([1,2,3,4]))
