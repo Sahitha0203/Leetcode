81037957abcc2c20f470a3921b184c497f8e7f1e
Sahitha0203 <sahitha.mutakodoor@gmail.com>
2025-07-28 22:08:47 +0530

    Update RemovingDuplicates1.js
    This is by using set approach , but this is not a good approach , as the time  complexity is O(n).

--- Day-01/RemovingDuplicates1.js
+++ Day-01/RemovingDuplicates1.js
@@ -1,1 +1,9 @@
-
+var removeDuplicates = function(nums) {
+    let mySet=new Set();
+    for(let i=0;i<=nums.length;i++){
+        mySet.add(nums[i]);
+        }
+    nums=mySet;
+    return mySet;
+};
+console.log(removeDuplicates([1,1,2]));
